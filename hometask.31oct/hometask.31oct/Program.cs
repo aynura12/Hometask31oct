218f350fc29a2935ff5e03db4c8362c82a86b7d8
aynura12 <7m1bz3t@coe.du.az>
2022-10-31 12:08:46 -0700

    created 31octtask

---
+++ hometask.31oct/hometask.31oct/Program.cs
@@ -0,0 +1,58 @@
+﻿#region Average
+int number = 10 ;
+int sum = 0;
+int average = 0;
+int count = 0;
+if (number != 10)
+{ Console.WriteLine("Bize lazim olan 1-10 araligindaki reqemlerin ededi ortasidir,edediniz 10-a beraber olmalidir"); }
+else
+{
+    for (int i = 1; i < number; i++)
+    {
+
+        if (i % 2 == 0)
+        {
+            sum = sum + i;
+
+            count++;
+        }
+
+    }
+    average = sum / count;
+    Console.WriteLine(average);
+
+
+
+}
+
+#endregion
+#region
+int nmbr = -17;
+int rem = 0;
+int level = 0;
+
+while (nmbr != 0)
+{
+    rem = nmbr % 10;
+    nmbr = (nmbr - rem) / 10;
+    level = level + rem;
+}
+if (level < 0)
+{ 
+    Console.WriteLine(level = level * (-1));
+}
+
+else
+{
+    Console.WriteLine(level);
+}
+
+if (level == 0)
+{ Console.WriteLine("0 daxil ede bilmerik");
+}
+
+
+
+
+
+#endregion
